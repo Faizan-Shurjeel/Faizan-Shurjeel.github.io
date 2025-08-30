c0806e2b148abe1ea869b067857744e44f2add58
Muhammad Faizan Shurjeel <logicalbakwas@gmail.com>
2025-08-30 15:41:48 +0500

    ..

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   productionBrowserSourceMaps: false,
+  assetPrefix: "./",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
