60cc4ad9da36096c191f3f3f561411dc99116cde
Muhammad Faizan Shurjeel <logicalbakwas@gmail.com>
2025-08-30 15:34:04 +0500

    ..

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -593,7 +593,7 @@ export default function Home() {
             </div>
           </div>
           <div className="footer-bottom">
-            <p>&copy; 2023 Muhammad Faizan Shurjeel. All rights reserved.</p>
+            <p>&copy; 2026 Muhammad Faizan Shurjeel. All rights reserved.</p>
           </div>
         </div>
       </footer>
